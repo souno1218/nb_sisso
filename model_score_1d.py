1dfunc : add score2 , 2dfunc : split _fit,_score , add score2 , log_progress : fix overflow bug

model_score_1d.py
@@ -1,7 +1,5 @@
-from numba import njit,int64,float64
 import numpy as np
-from numba.types import string
-from numba.types import unicode_type
+from numba import njit
 
 #If error_model is available for jitclass, replace all of them with class
 
@@ -15,7 +13,7 @@ def debug_1d(x,y):
 @njit(error_model="numpy")#,fastmath=True)
 def sub_debug_1d_fit(x,y):
     mu=np.mean(x)
-    sigma=np.sqrt(np.sum((x-mu)**2))
+    sigma=np.sqrt(np.mean((x-mu)**2))+1e-300
     return mu,sigma
 
 @njit(error_model="numpy")#,fastmath=True)
@@ -23,7 +21,7 @@ def sub_debug_1d_score(x,y,mu,sigma):
     return np.abs((x[0]-mu)/sigma),0
 
 
-### LDA
+### LDA  ,  https://qiita.com/m1t0/items/06f2d07e626d1c4733fd
 @njit(error_model="numpy")#,fastmath=True)
 def LDA_1d(x,y):
     args=sub_LDA_1d_fit(x,y)
@@ -35,16 +33,19 @@ def sub_LDA_1d_fit(x,y):
     pi_F=np.sum(~y)
     mu_T=np.mean(x[y])
     mu_F=np.mean(x[~y])
-    sigma=(np.sum((x[y]-mu_T)**2)+np.sum((x[~y]-mu_F)**2))/y.shape[0]
+    sigma=(np.sum((x[y]-mu_T)**2)+np.sum((x[~y]-mu_F)**2))/y.shape[0]+1e-300
     return pi_T,pi_F,mu_T,mu_F,sigma
 
 @njit(error_model="numpy")
 def sub_LDA_1d_score(x,y,pi_T,pi_F,mu_T,mu_F,sigma):
     f=(mu_T-mu_F)*x-(mu_T-mu_F)*(mu_T+mu_F)/2+sigma*np.log(pi_T/pi_F)
-    return np.sum((f>=0)==y)/y.shape[0],0
+    score=np.sum((f>=0)==y)/y.shape[0]
+    #Kullback-Leibler Divergence , https://sucrose.hatenablog.com/entry/2013/07/20/190146
+    kl_d=((mu_T-mu_F)**2)/2/sigma
+    return score,kl_d
 
 
-### QDA
+### QDA  ,  https://qiita.com/m1t0/items/06f2d07e626d1c4733fd
 @njit(error_model="numpy")
 def QDA_1d(x,y):
     args=sub_QDA_1d_fit(x,y)
@@ -52,17 +53,21 @@ def QDA_1d(x,y):
 
 @njit(error_model="numpy")
 def sub_QDA_1d_fit(x,y):
-    mu_T,mu_F=np.mean(x[y]),np.mean(x[~y])
+    mu_T,mu_F=np.mean(x[y])+1e-300,np.mean(x[~y])+1e-300
     pi_T,pi_F=np.sum(y),np.sum(~y)
-    sigma_T=np.sum((x[y]-mu_T)**2)/(pi_T-1)
-    sigma_F=np.sum((x[~y]-mu_F)**2)/(pi_F-1)        
-    value2=(2*np.log(pi_T/pi_F)-np.log(np.abs(sigma_T/sigma_F)))
+    sigma_T=np.sum((x[y]-mu_T)**2)/(pi_T-1)+1e-300
+    sigma_F=np.sum((x[~y]-mu_F)**2)/(pi_F-1)+1e-300      
+    value2=(2*np.log(pi_T/pi_F)-np.log(sigma_T/sigma_F))
     return mu_T,mu_F,sigma_T,sigma_F,value2
 
 @njit(error_model="numpy")
 def sub_QDA_1d_score(x,y,mu_T,mu_F,sigma_T,sigma_F,value2):
     value=-((x-mu_T)**2/sigma_T)+(((x-mu_F))**2/sigma_F)+value2
-    return np.sum((value>0)==y)/y.shape[0],0
+    score=np.sum((value>0)==y)/y.shape[0]
+    #Kullback-Leibler Divergence , https://sucrose.hatenablog.com/entry/2013/07/20/190146
+    kl_d=(sigma_T+(mu_T-mu_F)**2)/2/sigma_F-0.5
+    kl_d+=np.log(sigma_F/sigma_T)/2
+    return np.sum((value>0)==y)/y.shape[0],kl_d
 
 ### Hull
 @njit(error_model="numpy")#,fastmath=True)
@@ -83,7 +88,8 @@ def sub_Hull_1d_score(x,y,min_T,max_T,min_F,max_F):
     TF[y]|=x[y]>max_F
     TF[~y]=min_T>x[~y]
     TF[~y]|=x[~y]>max_T
-    return np.mean(TF),0
+    len_overlap=min(max_T,max_F)-max(min_T,min_F)
+    return np.mean(TF),-len_overlap
 
 ### DT
 @njit(error_model="numpy")
@@ -109,16 +115,33 @@ def sub_DT_1d_fit(x,y):
     sort_x=x[sort_index[TF]]
     if score_FT>score_TF:
         index=np.argmax(cumsum)
-        first_area=True
+        area_0_predict=True
     else:
         index=np.argmin(cumsum)
-        first_area=False
-    border=(sort_x[index]+sort_x[index+1])/2
-    return border,first_area
+        area_0_predict=False
+    if sort_x.shape[0]!=index+1:
+        border=(sort_x[index]+sort_x[index+1])/2
+    else:
+        border=sort_x[index]
+    return border,area_0_predict
 
 @njit(error_model="numpy")
-def sub_DT_1d_score(x,y,border,first_area):
-    return (np.sum(x[y==first_area]<=border)+np.sum(x[y!=first_area]>border))/y.shape[0],0
+def sub_DT_1d_score(x,y,border,area_0_predict):
+    n_tot=x.shape[0]+1e-300
+    x_first_area=(x<=border)
+    n_first_area=np.sum(x_first_area)+1e-300
+    n_first_area_T=np.sum(y[x_first_area]==area_0_predict)+1e-300
+    x_second_area=(x>border)
+    n_second_area=np.sum(x_second_area)+1e-300
+    n_second_area_T=np.sum(y[x_second_area]!=area_0_predict)+1e-300
+    
+    score=(n_first_area_T+n_second_area_T)/n_tot
+    
+    entropy=-n_first_area_T*np.log2(n_first_area_T/n_first_area)/n_tot
+    entropy+=-(n_first_area-n_first_area_T)*np.log2((n_first_area-n_first_area_T+1e-300)/n_first_area)/n_tot
+    entropy+=-n_second_area_T*np.log2(n_second_area_T/n_second_area)/n_tot
+    entropy+=-(n_second_area-n_second_area_T)*np.log2((n_second_area-n_second_area_T+1e-300)/n_second_area)/n_tot
+    return score,-entropy
 
 ### make_CV_model
 def make_CV_model(sub_func_fit,sub_func_score,k=5,name=None):

model_score_2d.py
@@ -1,119 +1,172 @@
-import datetime
 import numpy as np
-from numba_progress import ProgressBar
-from numba import njit,prange,set_num_threads,get_thread_id
+from utils import jit_cov
+from numba import njit
 #https://qiita.com/m1t0/items/06f2d07e626d1c4733fd
 
+### QDA_2d
 @njit(error_model="numpy",fastmath=True)
-def jit_cov(X,ddof=1):
-    n=X.shape[1]
-    X_1=X[0,:]-np.sum(X[0,:])/n
-    X_2=X[1,:]-np.sum(X[1,:])/n
-    var_1=np.sum(X_1*X_1)/(n-ddof)#不偏分散、sklearnに準拠
-    var_2=np.sum(X_2*X_2)/(n-ddof)
-    cross_cov=np.sum(X_1*X_2)/(n-ddof)
-    return var_1,var_2,cross_cov
+def QDA_2d(X,Y):
+    args=sub_QDA_2d_fit(X,Y)
+    return sub_QDA_2d_score(X,Y,*args)
 
 @njit(error_model="numpy",fastmath=True)
-def QDA_2d(X,Y):#X.shape=(2,-1)
-    class1_X=X[:,Y]
-    class2_X=X[:,~Y]
-    class1_var_1,class1_var_2,class1_cov=jit_cov(class1_X)
-    class2_var_1,class2_var_2,class2_cov=jit_cov(class2_X)
-    det_cov1=class1_var_1*class1_var_2-class1_cov**2
-    det_cov2=class2_var_1*class2_var_2-class2_cov**2    
-
-    target_x_1=(X.T-np.array([np.mean(class1_X[0,:]),np.mean(class1_X[1,:])])).T
-    target_x_2=(X.T-np.array([np.mean(class2_X[0,:]),np.mean(class2_X[1,:])])).T
-    value1=class1_var_2*target_x_1[0,:]**2+class1_var_1*target_x_1[1,:]**2-2*class1_cov*target_x_1[0,:]*target_x_1[1,:]
-    value1/=det_cov1
-    value2=class2_var_2*target_x_2[0,:]**2+class2_var_1*target_x_2[1,:]**2-2*class2_cov*target_x_2[0,:]*target_x_2[1,:]
-    value2/=det_cov2
-    value3=2*(np.log(np.sum(Y))-np.log(np.sum(~Y)))-np.log(det_cov1)+np.log(det_cov2)
+def sub_QDA_2d_fit(X,Y):
+    classT_X=X[:,Y]
+    classF_X=X[:,~Y]
+    pi_T,pi_F=classT_X.shape[1]+1e-300,classF_X.shape[1]+1e-300
+    classT_var_1,classT_var_2,classT_cov=jit_cov(classT_X)
+    det_covT=classT_var_1*classT_var_2-classT_cov**2+1e-300
+    
+    classF_var_1,classF_var_2,classF_cov=jit_cov(classF_X)
+    det_covF=classF_var_1*classF_var_2-classF_cov**2+1e-300
+    
+    mean_T=np.array([np.mean(classT_X[0,:]),np.mean(classT_X[1,:])])
+    mean_F=np.array([np.mean(classF_X[0,:]),np.mean(classF_X[1,:])])
+    return pi_T,pi_F,classT_var_1,classT_var_2,classT_cov,classF_var_1,classF_var_2,classF_cov,mean_T,mean_F,det_covT,det_covF
+    
+@njit(error_model="numpy",fastmath=True)
+def sub_QDA_2d_score(X,Y,pi_T,pi_F,classT_var_1,classT_var_2,classT_cov,classF_var_1,classF_var_2,classF_cov,mean_T,mean_F,det_covT,det_covF):
+    target_x_1=(X.T-mean_T).T
+    target_x_2=(X.T-mean_F).T
+    value1=classT_var_2*target_x_1[0,:]**2+classT_var_1*target_x_1[1,:]**2-2*classT_cov*target_x_1[0,:]*target_x_1[1,:]
+    value2=classF_var_2*target_x_2[0,:]**2+classF_var_1*target_x_2[1,:]**2-2*classF_cov*target_x_2[0,:]*target_x_2[1,:]
+    value3=2*np.log(pi_T/pi_F)-np.log(np.abs(det_covT/det_covF+1e-300))
     value=(-value1+value2+value3)
-    return np.sum((value>0)==Y)/X.shape[1],0
+    score=np.sum((value>0)==Y)/X.shape[1]
+
+    #Kullback-Leibler Divergence , https://sucrose.hatenablog.com/entry/2013/07/20/190146
+    kl_d=np.log(np.abs(det_covF/det_covT+1e-300))/2-1
+    kl_d+=(classF_var_2*classT_var_1+classF_var_1*classT_var_2-2*classT_cov*classF_cov)/det_covF/2
+    dmean=mean_T-mean_F
+    kl_d+=(dmean[0]**2*classF_var_2+dmean[1]**2*classF_var_1-2*dmean[0]*dmean[1]*classF_cov)/det_covF/2
+    return np.sum((value>0)==Y)/X.shape[1],kl_d
+
+### LDA_2d
+@njit(error_model="numpy",fastmath=True)
+def LDA_2d(X,Y):
+    args=sub_LDA_2d_fit(X,Y)
+    return sub_LDA_2d_score(X,Y,*args)
+    
+@njit(error_model="numpy",fastmath=True)
+def sub_LDA_2d_fit(X,Y):
+    pi_T=np.sum(Y)+1e-300
+    pi_F=np.sum(~Y)+1e-300
+    classT_var_0,classT_var_1,classT_cov=jit_cov(X[:,Y],ddof=0)
+    classF_var_0,classF_var_1,classF_cov=jit_cov(X[:,~Y],ddof=0)
+    var_0=(pi_T*classT_var_0+pi_F*classF_var_0)/(pi_T+pi_F)
+    var_1=(pi_T*classT_var_1+pi_F*classF_var_1)/(pi_T+pi_F)
+    cov=(pi_T*classT_cov+pi_F*classF_cov)/(pi_T+pi_F)
+    mean_T_0,mean_T_1=np.mean(X[0,Y]),np.mean(X[1,Y])
+    mean_F_0,mean_F_1=np.mean(X[0,~Y]),np.mean(X[1,~Y])
+    dmean_0,dmean_1=mean_T_0-mean_F_0,mean_T_1-mean_F_1
+    c=(mean_F_0**2-mean_T_0**2)*var_1/2+(mean_F_1**2-mean_T_1**2)*var_0/2
+    c+=(mean_T_0*mean_T_1-mean_F_0*mean_F_1)*cov
+    c-=(var_0*var_1-cov**2)*(np.log(pi_F/pi_T))
+    return var_0,var_1,cov,dmean_0,dmean_1,c
 
 @njit(error_model="numpy",fastmath=True)
-def LDA_2d(X,Y):#xy.shape=(2,-1)
-    pi_0=np.sum(Y)
-    pi_1=np.sum(~Y)
-    class0_var_0,class0_var_1,class0_cov=jit_cov(X[:,Y],ddof=0)
-    class1_var_0,class1_var_1,class1_cov=jit_cov(X[:,~Y],ddof=0)
-    var_0=(pi_0*class0_var_0+pi_1*class1_var_0)/(pi_0+pi_1)
-    var_1=(pi_0*class0_var_1+pi_1*class1_var_1)/(pi_0+pi_1)
-    cov=(pi_0*class0_cov+pi_1*class1_cov)/(pi_0+pi_1)
-    mean_0_0=np.mean(X[0,Y])
-    mean_0_1=np.mean(X[1,Y])
-    mean_1_0=np.mean(X[0,~Y])
-    mean_1_1=np.mean(X[1,~Y])
-    value=((mean_0_0-mean_1_0)*var_1-(mean_0_1-mean_1_1)*cov)*X[0]
-    value+=((mean_0_1-mean_1_1)*var_0-(mean_0_0-mean_1_0)*cov)*X[1]
-    value+=(mean_1_0**2-mean_0_0**2)*var_1/2
-    value+=(mean_1_1**2-mean_0_1**2)*var_0/2
-    value+=(mean_0_0*mean_0_1-mean_1_0*mean_1_1)*cov
-    value-=(var_0*var_1-cov**2)*(np.log(pi_1/pi_0))
-    return np.sum((value>0)==Y)/X.shape[1],0
+def sub_LDA_2d_score(X,Y,var_0,var_1,cov,dmean_0,dmean_1,c):
+    a=(dmean_0*var_1-dmean_1*cov)
+    b=(dmean_1*var_0-dmean_0*cov)    
+    score=np.sum(((a*X[0]+b*X[1]+c)>0)==Y)/X.shape[1]
+    #Kullback-Leibler Divergence , https://sucrose.hatenablog.com/entry/2013/07/20/190146
+    kl_d=(dmean_0**2*var_1+dmean_1**2*var_0-2*dmean_0*dmean_1*cov)/(var_0*var_1-cov**2+1e-300)/2
+    return score,kl_d
+
+### DT_2d
+from model_score_1d import DT_1d,sub_DT_1d_fit,sub_DT_1d_score
 
 @njit(error_model="numpy")
-def DT_1d(x,y):#x.shape=(N),y=ans
-    sort_index=np.argsort(x)
-    sorted_y=y[sort_index].astype("int64")*2-1
-    TF=np.empty(x.shape[0],dtype="bool")
-    TF[0]=True
-    TF[1:]=(x[sort_index[1:]]!=x[sort_index[:-1]])
-    if np.all(TF):
-        cumsum=np.cumsum(sorted_y)
-    else:
-        for i in np.arange(y.shape[0])[~TF][::-1]:
-            sorted_y[i-1]+=sorted_y[i]
-        cumsum=np.cumsum(sorted_y[TF])
-    return max(np.sum(~y)+np.max(cumsum),np.sum(y)-np.min(cumsum))/y.shape[0]
-
+def DT_2d(X,Y):
+    args=sub_DT_2d_fit(X,Y)
+    return sub_DT_2d_score(X,Y,*args)
+    
 @njit(error_model="numpy")
-def DT_1d_with_border(x,y):#x.shape=(N),y=ans
-    sort_index=np.argsort(x)
-    sorted_y=y[sort_index].astype("int64")*2-1
-    TF=np.empty(x.shape[0],dtype="bool")
-    TF[0]=True
-    TF[1:]=(x[sort_index[1:]]!=x[sort_index[:-1]])
-    if np.all(TF):
-        cumsum=np.cumsum(sorted_y)
-    else:
-        for i in np.arange(y.shape[0])[~TF][::-1]:
-            sorted_y[i-1]+=sorted_y[i]
-        cumsum=np.cumsum(sorted_y[TF])
-    score_FT=np.sum(~y)+np.max(cumsum)
-    score_TF=np.sum(y)-np.min(cumsum)
-    sort_x=x[sort_index[TF]]
-    if score_FT>score_TF:
-        index=np.argmax(cumsum)
-        score=score_FT/y.shape[0]
-    else:
-        index=np.argmin(cumsum)
-        score=score_TF/y.shape[0]
-    border=(sort_x[index]+sort_x[index+1])/2
-    return score,border
-
-@njit(error_model="numpy")
-def DT_2d(X,Y):#xy.shape=(2,-1)
-    score0,border0=DT_1d_with_border(X[0],Y)
-    score1,border1=DT_1d_with_border(X[1],Y)
-    arange=np.arange(Y.shape[0])
-    score=0
+def sub_DT_2d_fit(x,y):
+    #root node
+    border0,first_area_predict0=sub_DT_1d_fit(x[0],y)
+    score0,minus_entropy0=sub_DT_1d_score(x[0],y,border0,first_area_predict0)
+    border1,first_area_predict1=sub_DT_1d_fit(x[1],y)
+    score1,minus_entropy1=sub_DT_1d_score(x[1],y,border1,first_area_predict1)
     if score0>score1:
-        split_dim,score_dim,border=0,1,border0
+        root_node=0
+        root_border=border0
+    elif score0==score1:
+        if minus_entropy0>minus_entropy1:
+            root_node=0
+            root_border=border0
+        else:
+            root_node=1
+            root_border=border1
     else:
-        split_dim,score_dim,border=1,0,border1
-    index_0=arange[X[split_dim]>border]
-    index_1=arange[X[split_dim]<=border]
-    weight0=index_0.shape[0]/Y.shape[0]
-    weight1=index_1.shape[0]/Y.shape[0]
-    if weight0!=0:
-        score+=weight0*DT_1d(X[score_dim][index_0],Y[index_0])
-    if weight1!=0:
-        score+=weight1*DT_1d(X[score_dim][index_1],Y[index_1])
-    return score,0
+        root_node=1
+        root_border=border1
+    area_0_index=x[root_node]<root_border
+    area_1_index=x[root_node]>=root_border
+    
+    #leaf node  (area_0)
+    border0,area_0_predict0=sub_DT_1d_fit(x[0,area_0_index],y[area_0_index])
+    score0,minus_entropy0=sub_DT_1d_score(x[0,area_0_index],y[area_0_index],border0,first_area_predict0)
+    border1,area_0_predict1=sub_DT_1d_fit(x[1,area_0_index],y[area_0_index])
+    score1,minus_entropy1=sub_DT_1d_score(x[1,area_0_index],y[area_0_index],border1,first_area_predict1)
+    if score0>score1:
+        leaf_0_node=0
+        leaf_0_border=border0
+        leaf_0_area_0_predict=area_0_predict0
+    elif score0==score1:
+        if minus_entropy0>minus_entropy1:
+            leaf_0_node=0
+            leaf_0_border=border0
+            leaf_0_area_0_predict=area_0_predict0
+        else:
+            leaf_0_node=1
+            leaf_0_border=border1
+            leaf_0_area_0_predict=area_0_predict1
+    else:
+        leaf_0_node=1
+        leaf_0_border=border1
+        leaf_0_area_0_predict=area_0_predict1
+        
+    #leaf node  (area_1)
+    border0,area_0_predict0=sub_DT_1d_fit(x[0,area_1_index],y[area_1_index])
+    score0,minus_entropy0=sub_DT_1d_score(x[0,area_1_index],y[area_1_index],border0,first_area_predict0)
+    border1,area_0_predict1=sub_DT_1d_fit(x[1,area_1_index],y[area_1_index])
+    score1,minus_entropy1=sub_DT_1d_score(x[1,area_1_index],y[area_1_index],border1,first_area_predict1)
+    if score0>score1:
+        leaf_1_node=0
+        leaf_1_border=border0
+        leaf_1_area_0_predict=area_0_predict0
+    elif score0==score1:
+        if minus_entropy0>minus_entropy1:
+            leaf_1_node=0
+            leaf_1_border=border0
+            leaf_1_area_0_predict=area_0_predict0
+        else:
+            leaf_1_node=1
+            leaf_1_border=border1
+            leaf_1_area_0_predict=area_0_predict1
+    else:
+        leaf_1_node=1
+        leaf_1_border=border1
+        leaf_1_area_0_predict=area_0_predict1
+    return root_node,root_border,leaf_0_node,leaf_0_border,leaf_0_area_0_predict,leaf_1_node,leaf_1_border,leaf_1_area_0_predict
 
+@njit(error_model="numpy")
+def sub_DT_2d_score(x,y,root_node,root_border,leaf_0_node,leaf_0_border,leaf_0_area_0_predict,leaf_1_node,leaf_1_border,leaf_1_area_0_predict):
+    area_0_index=x[root_node]<root_border
+    n_area_0=np.sum(area_0_index)
+    area_1_index=x[root_node]>=root_border
+    n_area_1=np.sum(area_1_index)
+
+    score0,minus_entropy0=sub_DT_1d_score(x[leaf_0_node,area_0_index],y[area_0_index],leaf_0_border,leaf_0_area_0_predict)
+    score1,minus_entropy1=sub_DT_1d_score(x[leaf_1_node,area_1_index],y[area_1_index],leaf_1_border,leaf_1_area_0_predict)
+    
+    score=(n_area_0*score0+n_area_1*score1)/(n_area_0+n_area_1)
+    minus_entropy=(n_area_0*minus_entropy0+n_area_1*minus_entropy1)/(n_area_0+n_area_1)
+    return score,minus_entropy
+
+
+### Hull_2d
 @njit(error_model="numpy")
 def sub_Hull_2d(base_x,other_x,not_is_in,arange,base_index1,base_index2,base_x_mask):
     if np.any(not_is_in):
@@ -144,27 +197,27 @@ def Hull_2d(X,Y):#xy.shape=(2,-1)
     r_R=np.abs(1-6*np.sum((index0-index1)**2)/(n*(n**2-1)))
     if r_R>0.9:
         return 0,0
-    class1_X,class2_X=X[:,Y],X[:,~Y]
-    index_x_max=np.argmax(class1_X[0])
-    index_x_min=np.argmin(class1_X[0])
-    arange=np.arange(class1_X.shape[1])
-    class1_X_mask=np.ones(class1_X.shape[1],dtype="bool")
-    not_is_in=np.ones(class2_X.shape[1],dtype="bool")
-    class1_X_mask[index_x_max]=False
-    class1_X_mask[index_x_min]=False
-    copy_class1_X_mask=class1_X_mask.copy()
-    sub_Hull_2d(class1_X,class2_X,not_is_in,arange,index_x_max,index_x_min,copy_class1_X_mask)
-    sub_Hull_2d(class1_X,class2_X,not_is_in,arange,index_x_min,index_x_max,class1_X_mask)
+    classT_X,classF_X=X[:,Y],X[:,~Y]
+    index_x_max=np.argmax(classT_X[0])
+    index_x_min=np.argmin(classT_X[0])
+    arange=np.arange(classT_X.shape[1])
+    classT_X_mask=np.ones(classT_X.shape[1],dtype="bool")
+    not_is_in=np.ones(classF_X.shape[1],dtype="bool")
+    classT_X_mask[index_x_max]=False
+    classT_X_mask[index_x_min]=False
+    copy_classT_X_mask=classT_X_mask.copy()
+    sub_Hull_2d(classT_X,classF_X,not_is_in,arange,index_x_max,index_x_min,copy_classT_X_mask)
+    sub_Hull_2d(classT_X,classF_X,not_is_in,arange,index_x_min,index_x_max,classT_X_mask)
     ans=np.sum(~not_is_in)
-    index_x_max=np.argmax(class2_X[0])
-    index_x_min=np.argmin(class2_X[0])
-    arange=np.arange(class2_X.shape[1])
-    class2_X_mask=np.ones(class2_X.shape[1],dtype="bool")
-    not_is_in=np.ones(class1_X.shape[1],dtype="bool")
-    class2_X_mask[index_x_max]=False
-    class2_X_mask[index_x_min]=False
-    copy_class2_X_mask=class2_X_mask.copy()
-    sub_Hull_2d(class2_X,class1_X,not_is_in,arange,index_x_max,index_x_min,copy_class2_X_mask)
-    sub_Hull_2d(class2_X,class1_X,not_is_in,arange,index_x_min,index_x_max,class2_X_mask)
+    index_x_max=np.argmax(classF_X[0])
+    index_x_min=np.argmin(classF_X[0])
+    arange=np.arange(classF_X.shape[1])
+    classF_X_mask=np.ones(classF_X.shape[1],dtype="bool")
+    not_is_in=np.ones(classT_X.shape[1],dtype="bool")
+    classF_X_mask[index_x_max]=False
+    classF_X_mask[index_x_min]=False
+    copy_classF_X_mask=classF_X_mask.copy()
+    sub_Hull_2d(classF_X,classT_X,not_is_in,arange,index_x_max,index_x_min,copy_classF_X_mask)
+    sub_Hull_2d(classF_X,classT_X,not_is_in,arange,index_x_min,index_x_max,classF_X_mask)
     ans+=np.sum(~not_is_in)
     return 1-(ans/(Y.shape[0])),0